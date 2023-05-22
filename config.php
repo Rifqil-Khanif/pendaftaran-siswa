72ae4fb203d77638f5d45ffd3949b94061dc3b1d
Muhammad Rifqil Khanif <115907124+Rifqil-Khanif@users.noreply.github.com>
2023-05-22 19:19:55 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 
-$server = "localhost";
+$server = "containers-us-west-56.railway.app";
 $user = "root";
-$password = "";
-$nama_database = "pendaftaran_siswa";
+$password = "e1nj5D50lj6wpRDRpNwF";
+$nama_database = "railway";
+$port = '7020';
 
-$db = mysqli_connect($server, $user, $password, $nama_database);
+$db = mysqli_connect($server, $user, $password, $nama_database, $port);
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
